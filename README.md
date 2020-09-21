fbc52d9df84ecff751814abee0d5cd8a48a8e2e4
LWRGitHub <LoganReynoldsLWR@gmail.com>
2020-09-20 20:42:23 -0700

    updates with for loop & more complex stuff

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # cs10-chat-bot
- Python chat bot
 
--has a Function that response to how you say you feel depending on if you are happy or sad & some other cases
--ther is a greeting asking how you feel
--then there is a while loop that will
+# fisrt you are gretted
+
+# Then you enter a string of a feeling like 'happy' or a sentence like 'I am feeling happy'
+this will then..
+-Run a Function that response to how you say you feel depending on if you are happy or sad & some other cases
+-it will respond with a measage 
+
+# Mean while we are in a while loop
+-this ends when you enter 'done'
+
+# the streck chaleges are in here also 
+-1 it can take any case meaning it is not case sensetive
+-2 it can take a sentence as well
--- app.py
+++ app.py
@@ -1,26 +1,34 @@
 import random
 
+user_response = ""
+
 # Function that response to what you say
 def get_bot_response(user_response):
-    res = user_response.upper()
 
-    good_res = [f"I'm so glad you are {user_response}", f"I makes me feel {user_response} you are {user_response}!", f"{user_response} to hear your {user_response}!"]
-    bad_res = ["Well at least your above the ground!", f"I'm sorry to here your feeling {user_response} I hope your end to the day is better!", "Well be sure & do something for you self today!"]
+    for word in user_response:
+        res = word.upper()
 
-    if res == 'HAPPY' or res == 'GOOD' or res == "NICE":
-        return random.choice(good_res)
-    elif res == 'SAD' or res == 'BAD' or res == "MISERABLE":
-        return random.choice(bad_res)
-    else:
-        return "As the wise man once said the camle with the biges hump is the wealthiest."
+        if res == 'HAPPY' or res == 'GOOD' or res == "NICE" or res == 'SAD' or res == 'BAD' or res == "MISERABLE":
+
+            good_res = [f"I'm so glad you are {word}", f"I makes me feel {word} you are {word}!", f"{word} to hear your {word}!"]
+            bad_res = ["Well at least your above the ground!", f"I'm sorry to here your feeling {word} I hope your end to the day is better!", "Well be sure & do something for you self today!"]
+
+            
+            if res == 'HAPPY' or res == 'GOOD' or res == "NICE":
+                return random.choice(good_res)
+            elif res == 'SAD' or res == 'BAD' or res == "MISERABLE":
+                return random.choice(bad_res)
+        
+    return "As the wise man once said the camle with the biges hump is the wealthiest."
 
 #Greeting
 print("Hey there! Wecome welcome, come and tell me how you are feeling?")
 
-user_response = ""
+
 
 #keep asking how they feel tell user says done
-while user_response != 'done':
-    user_response = input('type "done" when you nologer want to talk, but pleas do tell me how you are felling?')
+while user_response != ['done']:
+    user_response = input('type "done" when you nologer want to talk, but pleas do tell me how you are felling?').split()
+
 
     print(get_bot_response(user_response))
